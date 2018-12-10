494261827774a2c09a687d75975ce1b364b11c40
Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>
2018-12-11 01:16:31 +0700

    vendor: Fix mistype
    Signed-off-by: Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyberAOSP-Project/vendor_cyber_aosp/pie/cyberaosp.devices | sed -e 's/#.*$//' | awk '{printf "lluvia_%s\n", $1, $2}')
+for combo in $(curl -s https://raw.githubusercontent.com/CyberAOSP-Project/vendor_cyber_aosp/pie/cyberaosp.devices | sed -e 's/#.*$//' | awk '{printf "cyber_%s\n", $1, $2}')
 do
     add_lunch_combo $combo-userdebug;
 done
