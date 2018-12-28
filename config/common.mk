ArrowOS/android_vendor_arrow

Copy the local init.rc to /etc

config/common.mk
@@ -26,6 +26,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
 
+	+
+# init file
+PRODUCT_COPY_FILES += \
+    vendor/arrow/prebuilt/common/etc/init.local.rc:root/init.arrow.rc
+
 # Packages
 include vendor/arrow/config/packages.mk
 

CyberAOSP-Project/android_vendor_cyber

Change-Id: Iae20a4b5293f68cf8cd1b17ecc493f966e6b6a73
Signed-off-by: BijiOnta <sengolcaduk32@gmail.com>

config/common.mk
@@ -211,4 +211,8 @@ PRODUCT_PACKAGES += \
     YellowAccent \
     WhiteAccent
 
+# init file
+PRODUCT_COPY_FILES += \
+    vendor/cyber/prebuilt/common/etc/init.local.rc:root/init.cyber.rc
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk